36beeb293565efb9682bbf1cc31dab14ea73fb2e
ledvina.m <ledvina.m@gmail.com>
2012-03-23 07:55:59 +0000

    StreamLoader commit, trying to figure out how to feed CSharpSyth classes with streams.

--- src/StreamLoader.cs
+++ src/StreamLoader.cs
@@ -1,19 +1,51 @@
+//#define MONODEVELOP
+//#define VISUALSTUDIO
+//#define UNITY
+//#define XNA
+
 using System;
 using System.IO;
-/*
- *	Every solution has to have its own c# file StreamLoader.cs and in it StreamLoaderDelegate in order to fill the needs for CSharpSyth stream needs. 
- *	All CSharpSynth should call this delegate in order to get the streams on different platform or engines or development tool targets.
- * 
- * 
- * 
- */
+#if UNITY
+using UnityEngine;
+#endif
+
+/* All CSharpSynth should call this delegate in order to get the streams on different platform or engines or development tool targets. */
+ 
 namespace CSharpSynth
 {
 	static public class StreamLoader
 	{
+		
+#if !MONODEVELOP && !VISUALSTUDIO && !UNITY && !XNA
 		static public Stream StreamLoaderDelegate(string aPath){
 			return null;	
 		}
+#endif
+		
+#if MONODEVELOP
+		static public Stream StreamLoaderDelegate(string aPath){
+			return null;	
+		}
+#endif
+		
+#if VISUALSTUDIO
+		static public Stream StreamLoaderDelegate(string aPath){
+			return null;	
+		}
+#endif
+
+#if UNITY
+		static public Stream StreamLoaderDelegate(string aPath){
+			return null;	
+		}
+#endif
+		
+#if XNA
+		static public Stream StreamLoaderDelegate(string aPath){
+			return null;	
+		}
+#endif
+		
 	}
 }
 
