dab16ed4c50dd1f438815458b7b94365bdb2ba05
csharpproj@gmail.com <csharpproj@gmail.com@7e64c04e-eafd-0cdb-4853-a089f8003a6b>
2012-03-23 18:02:56 +0000

    WaveFile: 1. added properties Data and Format 2. changed SampledData property was incorrect when editing the samples
    WaveFileWriter:
    1. filepath was incorrect and never created temp file for storage
    2. filereader should have been writer in last case
    
    WaveHelper:
    1. got rid of an unused variable in change bits per sample

--- trunk/src/Wave/WaveFile.cs
+++ trunk/src/Wave/WaveFile.cs
@@ -3,41 +3,51 @@
     public class WaveFile
     {
         //--Variables
-        private IChunk[] WaveChunks;
-        private byte[] _data;
+        private IChunk[] waveChunks;
+        private DataChunk dataChunk;
+        private FormatChunk fmtChunk;
         //--Public Methods
         public WaveFile(IChunk[] WaveChunks)
         {
-            this.WaveChunks = WaveChunks;
-            _data = ((DataChunk)GetChunk(WaveHelper.WaveChunkType.Data)).sampled_data;
+            this.waveChunks = WaveChunks;
+            this.dataChunk = (DataChunk)GetChunk(WaveHelper.WaveChunkType.Data);
+            this.fmtChunk = (FormatChunk)GetChunk(WaveHelper.WaveChunkType.Format);
         }
         public IChunk GetChunk(WaveHelper.WaveChunkType ChunkType)
         {
-            for (int x = 0; x < WaveChunks.Length; x++)
+            for (int x = 0; x < waveChunks.Length; x++)
             {
-                if (WaveChunks[x].GetChunkType() == ChunkType)
-                    return WaveChunks[x];
+                if (waveChunks[x].GetChunkType() == ChunkType)
+                    return waveChunks[x];
             }
             return null;
         }
         public IChunk GetChunk(int startIndex, WaveHelper.WaveChunkType ChunkType)
         {
-            if (startIndex >= WaveChunks.Length)
+            if (startIndex >= waveChunks.Length)
                 return null;
-            for (int x = startIndex; x < WaveChunks.Length; x++)
+            for (int x = startIndex; x < waveChunks.Length; x++)
             {
-                if (WaveChunks[x].GetChunkType() == ChunkType)
-                    return WaveChunks[x];
+                if (waveChunks[x].GetChunkType() == ChunkType)
+                    return waveChunks[x];
             }
             return null;
         }
+        public DataChunk Data
+        {
+            get { return dataChunk; }
+        }
+        public FormatChunk Format
+        {
+            get { return fmtChunk; }
+        }
         public byte[] SampleData
         {
-            get { return _data; }
+            get { return dataChunk.sampled_data; }
         }
         public int NumberOfChunks
         {
-            get { return WaveChunks.Length; }
+            get { return waveChunks.Length; }
         }
     }
 }
