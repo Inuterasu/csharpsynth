22d88e70a1e6ee50a799c72a121bf53582bf5b3c
ledvina.m@gmail.com <ledvina.m@gmail.com@7e64c04e-eafd-0cdb-4853-a089f8003a6b>
2012-04-01 06:03:25 +0000

    Queuing midi server messages.

--- branches/V1.0/src/CSharpSynth/Synthesis/StreamSynthesizer.MidiServer.cs
+++ branches/V1.0/src/CSharpSynth/Synthesis/StreamSynthesizer.MidiServer.cs
@@ -16,72 +16,96 @@ namespace CSharpSynth.Synthesis
 {
     public partial class StreamSynthesizer
     {	
-        private void ShortMessage(int aCommand, int aData1, int aData2)
+        private struct ShortMessageStruct
+	    {
+	        public int command;
+	        public int data1;
+	        public int data2;
+	     	
+	        public ShortMessageStruct(int command, int data1, int data2)
+	        {
+	            this.command = command;
+	            this.data1 = data1;
+	            this.data2 = data2;
+	        }
+	    }
+		
+		Stack<ShortMessageStruct> servermessages_ = new Stack<ShortMessageStruct>(400);
+		
+		public void ShortMessage(int aCommand, int data1, int data2)
         {
-			
-			int channel = (aCommand & 0xF);
-			int command = (aCommand >> 4);
-			
-			switch (command)
-            {
-                case 0x08: //NoteOff
-                  	NoteOff(channel, aData1);
-	                break;
-                case 0x09: //NoteOn
-                	if(aData2 == 0) NoteOff(channel, aData1);
-					else NoteOn(channel, aData1, aData2, instruments_[channel]);
-                    break;
-                case 0x0A: //NoteAftertouch
-                    break;
-                case 0x0B: //Controller
-                    {				
-						switch(aData1){
-                        case 0x7B: //Note Off All
-							NoteOffAll(true);						
-							break;
-                        case 0x07: //Channel Volume
-							volPositions_[channel] = MidiHelper.GetLogarithmicVolume(aData2);						
-							break;
-                        case 0x0A: //Pan
-							panPositions_[channel] = (aData2 - 64) == 63 ? 1.00f : (aData2 - 64) / 64.0f;						
-							break;
-                        case 0x01: //Modulation
-							vibraPositions_[channel] = (aData2 / 127.0) / 20.0;						
-							break;
-                        case 0x64: //Fine Select
-                            RPF[channel] = (byte)aData2;
-							break;
-                        case 0x65: //Coarse Select
-                            RPC[channel] = (byte)aData2;
-							break;
-                        case 0x06: // DataEntry Coarse
-                            if (RPC[channel] == 0)//change semitone
-                                pitchWheelSemitoneRange_[channel] = pitchWheelSemitoneRange_[channel] - ((int)pitchWheelSemitoneRange_[channel]) + aData2;                          
-                            break;
-                        case 0x26: // DataEntry Fine
-                            if (RPF[channel] == 0)//change cents
-                                pitchWheelSemitoneRange_[channel] = ((int)pitchWheelSemitoneRange_[channel]) + (aData2 / 100.0);
-                            break;
-                        case 0x79: // Reset All
-                            resetSynthControls();
-                            break;
-                        default:
-                            return;
-						}
-                    }
-                    break;
-                case 0x0C: //Program Change
-                    instruments_[channel] = aData1;
-                    break;
-                case 0x0D: //Channel Aftertouch
-                    break;
-                case 0x0E: //pitch bend
-                    //pitchbend is -1 to 1 and effects tune in the semitone range given by the pitchwheel
-                    int s = ((aData1 << 7) | aData2); // 0 to 16383
-                    tunePositions_[channel] = ((s - 8192.0) / 8192.0) * pitchWheelSemitoneRange_[channel];
-                    break;
-                default:
-                    return;
+			servermessages_.Push(new ShortMessageStruct{command = aCommand, data1 = shortMessage.data1, data2 = shortMessage.data2});
+		}
+		
+		
+		ShortMessageStruct shortMessage;
+		private void ProcessServerMessages(){
+			while(servermessages_.Count > 0){
+				shortMessage = servermessages_.Pop();
+				int channel = (shortMessage.command & 0xF);
+				int command = (shortMessage.command >> 4);
+				
+				switch (command)
+	            {
+	                case 0x08: //NoteOff
+	                  	NoteOff(channel, shortMessage.data1);
+		                break;
+	                case 0x09: //NoteOn
+	                	if(shortMessage.data2 == 0) NoteOff(channel, shortMessage.data1);
+						else NoteOn(channel, shortMessage.data1, shortMessage.data2, instruments_[channel]);
+	                    break;
+	                case 0x0A: //NoteAftertouch
+	                    break;
+	                case 0x0B: //Controller
+	                    {				
+							switch(shortMessage.data1){
+	                        case 0x7B: //Note Off All
+								NoteOffAll(true);						
+								break;
+	                        case 0x07: //Channel Volume
+								volPositions_[channel] = MidiHelper.GetLogarithmicVolume(shortMessage.data2);						
+								break;
+	                        case 0x0A: //Pan
+								panPositions_[channel] = (shortMessage.data2 - 64) == 63 ? 1.00f : (shortMessage.data2 - 64) / 64.0f;						
+								break;
+	                        case 0x01: //Modulation
+								vibraPositions_[channel] = (shortMessage.data2 / 127.0) / 20.0;						
+								break;
+	                        case 0x64: //Fine Select
+	                            RPF[channel] = (byte)shortMessage.data2;
+								break;
+	                        case 0x65: //Coarse Select
+	                            RPC[channel] = (byte)shortMessage.data2;
+								break;
+	                        case 0x06: // DataEntry Coarse
+	                            if (RPC[channel] == 0)//change semitone
+	                                pitchWheelSemitoneRange_[channel] = pitchWheelSemitoneRange_[channel] - ((int)pitchWheelSemitoneRange_[channel]) + shortMessage.data2;                          
+	                            break;
+	                        case 0x26: // DataEntry Fine
+	                            if (RPF[channel] == 0)//change cents
+	                                pitchWheelSemitoneRange_[channel] = ((int)pitchWheelSemitoneRange_[channel]) + (shortMessage.data2 / 100.0);
+	                            break;
+	                        case 0x79: // Reset All
+	                            resetSynthControls();
+	                            break;
+	                        default:
+	                            return;
+							}
+	                    }
+	                    break;
+	                case 0x0C: //Program Change
+	                    instruments_[channel] = shortMessage.data1;
+	                    break;
+	                case 0x0D: //Channel Aftertouch
+	                    break;
+	                case 0x0E: //pitch bend
+	                    //pitchbend is -1 to 1 and effects tune in the semitone range given by the pitchwheel
+	                    int s = ((shortMessage.data1 << 7) | shortMessage.data2); // 0 to 16383
+	                    tunePositions_[channel] = ((s - 8192.0) / 8192.0) * pitchWheelSemitoneRange_[channel];
+	                    break;
+	                default:
+	                    return;
+				}
 			}
 		}
 	}
